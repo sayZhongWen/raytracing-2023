370b1119d181c994ccab30c9716459469c4b08ed
dizzy_d <dizzy_d@sjtu.edu.cn>
2023-06-23 18:45:25 +0800

    A blue-to-white gradient depending on ray Y coordinate

--- raytracer/src/ray.rs
+++ raytracer/src/ray.rs
@@ -7,7 +7,13 @@ impl Ray {
     pub fn new(orig: Vec3, dir: Vec3) -> Ray {
         Ray { orig, dir }
     }
-    pub fn orig(&self) -> Vec3 { self.orig.clone() }
-    pub fn dir(&self) -> Vec3 { self.dir.clone() }
-    pub fn at(&self, t: f64) -> Vec3 { self.orig.clone() + t * self.dir.clone() }
+    pub fn orig(&self) -> Vec3 {
+        self.orig.clone()
+    }
+    pub fn dir(&self) -> Vec3 {
+        self.dir.clone()
+    }
+    pub fn at(&self, t: f64) -> Vec3 {
+        self.orig.clone() + t * self.dir.clone()
+    }
 }
