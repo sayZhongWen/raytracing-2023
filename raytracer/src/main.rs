3eace9fe96d2d840fd37733137734b28d7014ddb
dizzy_d <dizzy_d@sjtu.edu.cn>
2023-06-23 17:47:42 +0800

    A blue-to-white gradient depending on ray Y coordinate

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,16 +1,24 @@
 mod color;
 mod vec3;
+mod ray;
 
 use color::write_color;
 use image::{ImageBuffer, RgbImage};
 use indicatif::ProgressBar;
 use std::fs::File;
 pub use vec3::Vec3;
+pub use crate::ray::Ray;
 
 const AUTHOR: &str = "Dizzy_D";
 
 fn is_ci() -> bool {
     option_env!("CI").unwrap_or_default() == "true"
+}
+
+fn ray_color(r: &Ray) -> Vec3{
+    let unit_direction: Vec3 = r.dir().unit_vector();
+    let t=0.5*(unit_direction.y()+1.0);
+    (1.0-t)*Vec3::new(1.0,1.0,1.0)+t*Vec3::new(0.5,0.7,1.0)
 }
 
 fn main() {
@@ -19,10 +27,23 @@ fn main() {
 
     println!("CI: {}", is_ci);
 
-    let height: usize = 800;
-    let width: usize = 800;
+    // let height: usize = 800;
+    // let width: usize = 800;
     let path = "output/test.jpg";
     let quality = 60; // From 0 to 100, suggested value: 60
+
+    //image
+    const ASPECT_RATIO: f64 = 16.0/9.0;
+    const width: usize = 400;
+    const height: usize = 225;
+    //camera
+    let viewpoint_height:f64=2.0;
+    let viewpoint_width=viewpoint_height*ASPECT_RATIO;
+    let focal_length=1.0;
+    let origin=Vec3::new(0.0,0.0,0.0);
+    let horizontal=Vec3::new(viewpoint_width,0.0,0.0);
+    let vertical=Vec3::new(0.0,viewpoint_height,0.0);
+    let lower_left_corner=origin.clone()-horizontal.clone()/2.0-vertical.clone()/2.0-Vec3::new(0.0,0.0,focal_length);
 
     // Create image data
     let mut img: RgbImage = ImageBuffer::new(width.try_into().unwrap(), height.try_into().unwrap());
@@ -38,11 +59,19 @@ fn main() {
 
     for j in 0..height {
         for i in 0..width {
-            let pixel_color = [
-                (j as f32 / height as f32 * 255.).floor() as u8,
-                ((i + height - j) as f32 / (height + width) as f32 * 255.).floor() as u8,
-                (i as f32 / height as f32 * 255.).floor() as u8,
+            let u=i as f64/(width-1) as f64;
+            let v=j as f64/(height-1) as f64;
+            let r=Ray::new(origin.clone(),lower_left_corner.clone()+horizontal.clone()*u+vertical.clone()*v-origin.clone());
+            let pixel_color=[
+                (ray_color(&r).x()*255.0).floor() as u8,
+                (ray_color(&r).y()*255.0).floor() as u8,
+                (ray_color(&r).z()*255.0).floor() as u8,
             ];
+            // let pixel_color = [
+            //     (j as f32 / height as f32 * 255.).floor() as u8,
+            //     ((i + height - j) as f32 / (height + width) as f32 * 255.).floor() as u8,
+            //     (i as f32 / height as f32 * 255.).floor() as u8,
+            // ];
             write_color(pixel_color, &mut img, i, height - j - 1);
             bar.inc(1);
         }
--- raytracer/src/vec3.rs
+++ raytracer/src/vec3.rs
@@ -1,6 +1,10 @@
-use std::ops::{Add, AddAssign};
+use std::f64;
+// use std::intrinsics::sqrtf64;
+// use std::num;
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign, Div, DivAssign};
 
 #[derive(Clone, Debug, PartialEq)]
+
 pub struct Vec3 {
     x: f64,
     y: f64,
@@ -32,6 +36,22 @@ impl Vec3 {
     pub fn squared_length(&self) -> f64 {
         self.x * self.x + self.y * self.y + self.z * self.z
     }
+
+    pub fn length(&self) -> f64{
+        self.squared_length().sqrt()
+    }
+
+    pub fn dot(&self,v: Vec3)->f64{ self.x*v.x+self.y*v.y+self.z*v.z }
+
+    pub fn cross(&self,v: Vec3)->Vec3{
+        Vec3{
+            x:self.y*v.z-self.z*v.y,
+            y:self.z*v.x-self.x*v.z,
+            z:self.x*v.y-self.y*v.x,
+        }
+    }
+
+    pub fn unit_vector(&self) -> Vec3{self.clone()/self.length()}
 }
 
 impl Add for Vec3 {
@@ -65,6 +85,100 @@ impl AddAssign for Vec3 {
             y: self.y + other.y,
             z: self.z + other.z,
         };
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Sub<f64> for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: f64) -> Self {
+        Self {
+            x: self.x - other,
+            y: self.y - other,
+            z: self.z - other,
+        }
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        };
+    }
+}
+impl Mul<Vec3> for f64{
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3{
+        Vec3{
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self {
+        Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+impl Mul<Vec3> for Vec3{
+    type Output = f64;
+    fn mul(self, rhs: Vec3) -> f64 {
+        self.dot(rhs)
+    }
+}
+impl MulAssign<f64> for Vec3{
+    fn mul_assign(&mut self, other: f64) {
+        *self=Self{
+            x:other*self.x,
+            y:other*self.y,
+            z:other*self.z,
+        }
+    }
+}
+impl MulAssign<Vec3> for Vec3{
+    fn mul_assign(&mut self, rhs: Vec3) {
+        *self=self.cross(rhs)
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+    fn div(self, rhs: f64) -> Self::Output {
+        Self{
+            x:self.x/rhs,
+            y:self.y/rhs,
+            z:self.z/rhs,
+        }
+    }
+}
+impl DivAssign<f64> for Vec3{
+    fn div_assign(&mut self, rhs: f64) {
+        *self=Self{
+            x:self.x/rhs,
+            y:self.y/rhs,
+            z:self.z/rhs,
+        }
     }
 }
 
